ab6c2e5a73e673b1341e4d57d51f7e2acee40e3b
Stefan Johnson <sj@scrimpycat.io>
2017-12-07 11:32:27 +1100

    Implement behaviours

--- lib/ex_doc_simple_markdown.ex
+++ lib/ex_doc_simple_markdown.ex
@@ -1,2 +1,20 @@
 defmodule ExDocSimpleMarkdown do
+    @behaviour ExDoc.Markdown
+
+    @impl ExDoc.Markdown
+    def to_html(input, _) do
+        SimpleMarkdown.convert(input)
+    end
+
+    @impl ExDoc.Markdown
+    def assets(_), do: []
+
+    @impl ExDoc.Markdown
+    def before_closing_head_tag(_), do: ""
+
+    @impl ExDoc.Markdown
+    def before_closing_body_tag(_), do: ""
+
+    @impl ExDoc.Markdown
+    def configure(_), do: :ok
 end
