e8f367bdaee3b7e156e00bbb9500adeb4bc834c1
Stefan Johnson <sj@scrimpycat.io>
2017-12-07 15:33:43 +1100

    Optionally disable pretty codeblocks

--- lib/ex_doc_simple_markdown.ex
+++ lib/ex_doc_simple_markdown.ex
@@ -4,12 +4,14 @@ defmodule ExDocSimpleMarkdown do
 
       Provides configurable options for the assets (`:assets`),
       before_closing_head_tag (`:head_tag`), before_closing_body_tag
-      (`:body_tag`) behaviours defined by ExDoc.
+      (`:body_tag`) behaviours defined by ExDoc. As well as the option
+      to enable or disable pretty codeblocks (`:pretty_codeblocks`).
 
         config :ex_doc_simple_markdown, [
                 assets: [{ "dist/hello-js.js", "alert('hello');" }],
                 head_tag: "<script src=\\"dist/hello-js.js\\"></script>",
-                body_tag: "<script>alert('goodbye');</script>"
+                body_tag: "<script>alert('goodbye');</script>",
+                pretty_codeblocks: false
             ]
     ]
     """
@@ -17,7 +19,15 @@ defmodule ExDocSimpleMarkdown do
     @behaviour ExDoc.Markdown
 
     @impl ExDoc.Markdown
-    def to_html(input, _), do: SimpleMarkdown.convert(input)
+    def to_html(input, _) do
+        html = SimpleMarkdown.convert(input)
+
+        if Application.get_env(:ex_doc_simple_markdown, :pretty_codeblocks, true) do
+            ExDoc.Markdown.pretty_codeblocks(html)
+        else
+            html
+        end
+    end
 
     @impl ExDoc.Markdown
     def assets(_), do: Application.get_env(:ex_doc_simple_markdown, :assets, [])
