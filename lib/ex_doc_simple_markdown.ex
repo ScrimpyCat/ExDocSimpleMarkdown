ccdcc8cfcb287933b32bfa64972ddcbc36b044e8
Stefan Johnson <sj@scrimpycat.io>
2017-12-07 11:43:18 +1100

    Expose other fields as configurables

--- lib/ex_doc_simple_markdown.ex
+++ lib/ex_doc_simple_markdown.ex
@@ -2,18 +2,16 @@ defmodule ExDocSimpleMarkdown do
     @behaviour ExDoc.Markdown
 
     @impl ExDoc.Markdown
-    def to_html(input, _) do
-        SimpleMarkdown.convert(input)
-    end
+    def to_html(input, _), do: SimpleMarkdown.convert(input)
 
     @impl ExDoc.Markdown
-    def assets(_), do: []
+    def assets(_), do: Application.get_env(:ex_doc_simple_markdown, :assets, [])
 
     @impl ExDoc.Markdown
-    def before_closing_head_tag(_), do: ""
+    def before_closing_head_tag(_), do: Application.get_env(:ex_doc_simple_markdown, :head_tag, "")
 
     @impl ExDoc.Markdown
-    def before_closing_body_tag(_), do: ""
+    def before_closing_body_tag(_), do: Application.get_env(:ex_doc_simple_markdown, :body_tag, "")
 
     @impl ExDoc.Markdown
     def configure(_), do: :ok
