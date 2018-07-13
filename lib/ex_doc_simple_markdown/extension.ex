e9d37e37e8aa58e4c69522f8aa6ae2c14f7f72cc
Stefan Johnson <sj@scrimpycat.io>
2018-07-13 15:49:54 +1000

    Fix missing callback argument

--- lib/ex_doc_simple_markdown/extension.ex
+++ lib/ex_doc_simple_markdown/extension.ex
@@ -8,15 +8,15 @@ defmodule ExDocSimpleMarkdown.Extension do
     @doc """
       A callback to modify the rule list before converting.
     """
-    @callback rules(rules :: [Parsey.rule]) :: [Parsey.rule]
+    @callback rules(rules :: [Parsey.rule], opts :: any) :: [Parsey.rule]
 
     @doc """
       A callback to modify the text input.
     """
-    @callback input(text :: String.t) :: String.t
+    @callback input(text :: String.t, opts :: any) :: String.t
 
     @doc """
       A callback to modify the HTML output.
     """
-    @callback output(html :: String.t) :: String.t
+    @callback output(html :: String.t, opts :: any) :: String.t
 end
