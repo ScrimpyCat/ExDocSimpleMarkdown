b813256389718d10d52d252fbd6cb487328a0053
Stefan Johnson <sj@scrimpycat.io>
2018-10-16 21:15:56 +1100

    Support new codeblock highlighter in ex_doc 0.19

--- lib/ex_doc_simple_markdown.ex
+++ lib/ex_doc_simple_markdown.ex
@@ -48,7 +48,11 @@ defmodule ExDocSimpleMarkdown do
         html = SimpleMarkdown.convert(input, parser: rules, render: renderer)
 
         html = if Application.get_env(:ex_doc_simple_markdown, :pretty_codeblocks, true) do
-            ExDoc.Markdown.pretty_codeblocks(html)
+            if Version.match?(ExDoc.version, "> 0.19.0") do
+                apply(ExDoc.Highlighter, :highlight_code_blocks, [html])
+            else
+                apply(ExDoc.Markdown, :pretty_codeblocks, [html])
+            end
         else
             html
         end
