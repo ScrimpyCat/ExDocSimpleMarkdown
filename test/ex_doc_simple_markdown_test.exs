d64855d08a6c98b64a2decd04e2c46279ff728af
Stefan Johnson <sj@scrimpycat.io>
2020-08-07 23:52:05 +1000

    Test ex_doc

--- test/ex_doc_simple_markdown_test.exs
+++ test/ex_doc_simple_markdown_test.exs
@@ -1,4 +1,16 @@
 defmodule ExDocSimpleMarkdownTest do
     use ExUnit.Case
     doctest ExDocSimpleMarkdown
+
+    test "ex_doc" do
+        assert ExDocSimpleMarkdown != ExDoc.Markdown.get_markdown_processor()
+        ExDoc.Markdown.put_markdown_processor(ExDocSimpleMarkdown)
+        assert ExDocSimpleMarkdown == ExDoc.Markdown.get_markdown_processor()
+
+        if Version.match?(ExDoc.version, ">= 0.22.0") do
+            assert { "h1", [], "foo" } == ExDoc.Markdown.to_ast("#foo")
+        else
+            assert "<h1>foo</h1>" == ExDoc.Markdown.to_html("#foo")
+        end
+    end
 end
